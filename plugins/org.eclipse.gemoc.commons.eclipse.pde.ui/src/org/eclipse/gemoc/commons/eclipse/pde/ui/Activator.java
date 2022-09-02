798e448fbf1c0c301999476312280bf608d6fc86
Didier Vojtisek <didier.vojtisek@inria.fr>
2022-09-02 15:38:09 +0200

    move UI feature out of commons.eclipse.pde plugin
    creates a new commons.eclipse.pde.ui plugin for features requiring UI

--- plugins/org.eclipse.gemoc.commons.eclipse.pde.ui/src/org/eclipse/gemoc/commons/eclipse/pde/ui/Activator.java
+++ plugins/org.eclipse.gemoc.commons.eclipse.pde.ui/src/org/eclipse/gemoc/commons/eclipse/pde/ui/Activator.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Inria - initial API and implementation
  *******************************************************************************/
-package org.eclipse.gemoc.commons.eclipse.pde;
+package org.eclipse.gemoc.commons.eclipse.pde.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -19,7 +19,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.gemoc.commons.eclipse.pde"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.gemoc.commons.eclipse.pde.ui"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
